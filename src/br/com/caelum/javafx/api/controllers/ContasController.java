af24ce43080343ce7389943bef1dcd0478c314d4
Joviane <joviane@gmail.com>
2016-05-09 15:48:06 -0300

    Populando os campos da tela com os dados da Conta

--- src/br/com/caelum/javafx/api/controllers/ContasController.java
+++ src/br/com/caelum/javafx/api/controllers/ContasController.java
@@ -1,74 +1,127 @@
 package br.com.caelum.javafx.api.controllers;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import com.sun.xml.internal.ws.util.StringUtils;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import br.com.caelum.javafx.api.util.Campos;
 import br.com.caelum.javafx.api.util.Evento;
 
 public class ContasController {
 	
-	private static final String PACOTE = "br.com.caelum.contas.";
+	private static final String CLASSE_CONTA = "br.com.caelum.contas.modelo.Conta";
+	private static final String PACOTE_BASE = "br.com.caelum.contas.";
+	private static final String PACOTE_MODELO = PACOTE_BASE + "modelo.";
 	private static final String MANIPULADOR_DE_CONTAS = "ManipuladorDeContas";
 
 	@FXML
-	private TextField valor;
+	private Label valor;
 	
 	@FXML
-	private TextField numero;
+	private Label numero;
 
 	@FXML
-	private TextField agencia;
+	private Label agencia;
 
 	@FXML
-	private TextField titular;
+	private Label titular;
+
+	private Object manipuladorDeContas;
 	
 	@FXML
 	public void initialize(){
 		Campos.registraCampos(this);
+		criaManipuladorDeContas();
 	}
 	
+	@FXML
 	public void criaConta(){
-		
+		invocaMetodo("criaConta");
+		recuperaConta();
 	}
-	
+
+	@FXML
 	public void saca(ActionEvent event){
 		invocaMetodo("saca");
 	}
 	
+	@FXML
 	public void deposita(ActionEvent event){
 		invocaMetodo("deposita");
 	}
 
+	@FXML
 	public void transfere(ActionEvent event){
 		invocaMetodo("transfere");
 	}
 
-	private void invocaMetodo(String nomeDoMetodo) {
+	private void criaManipuladorDeContas() {
 		try {
-			Class<?> classe = Class.forName(PACOTE + MANIPULADOR_DE_CONTAS);
-			Object manipuladorDeContas = classe.newInstance();
-			Method metodo = classe.getMethod(nomeDoMetodo, Evento.class);
-			Evento evento = new Evento();
-			metodo.invoke(manipuladorDeContas, evento);
-			
+			Class<?> classe = Class.forName(PACOTE_BASE + MANIPULADOR_DE_CONTAS);
+			this.manipuladorDeContas = classe.newInstance();
 		} catch (ClassNotFoundException e) {
-			throw new RuntimeException("Não foi encontrada a classe " + MANIPULADOR_DE_CONTAS + " no pacote " + PACOTE + " Verifique se o pacote e o nome da classe estão corretos.");
+			throw new RuntimeException("Não foi encontrada a classe " + MANIPULADOR_DE_CONTAS + " no pacote " + PACOTE_BASE + " Verifique se o pacote e o nome da classe estão corretos.");
 		} catch (InstantiationException e) {
 			throw new RuntimeException("Não foi possível chamar a classe " + MANIPULADOR_DE_CONTAS + ". Verifique se a sua classe possui um construtor sem argumentos.");
 		} catch (IllegalAccessException e) {
 			throw new RuntimeException("Não foi possível chamar a classe " + MANIPULADOR_DE_CONTAS + ". Verifique se a sua classe possui um construtor sem argumentos público.");
+		}
+	}
+
+	private void invocaMetodo(String nomeDoMetodo) {
+		try {
+			Method metodo = this.manipuladorDeContas.getClass().getMethod(nomeDoMetodo, Evento.class);
+			Evento evento = new Evento();
+			metodo.invoke(manipuladorDeContas, evento);
 		} catch (NoSuchMethodException e) {
 			throw new RuntimeException("Não foi encontrado o método " + nomeDoMetodo + " com o parâmetro do tipo Evento dentro da classe " + MANIPULADOR_DE_CONTAS + ". Verifique se o método foi criado corretamente.");
-		} catch (SecurityException e) {
+		} catch (SecurityException | IllegalAccessException e) {
 			throw new RuntimeException("Não foi possível chamar o método " + nomeDoMetodo + " com o parâmetro do tipo Evento dentro da classe " + MANIPULADOR_DE_CONTAS + ". Verifique se o método é público.");
 		} catch (IllegalArgumentException e) {
-			throw new RuntimeException("Oops, problemas internos. Chame o instrutor. '-'");
+			throw new RuntimeException("Oops, problemas internos. Chame o instrutor. '-' \nException: " + e.getMessage());
 		} catch (InvocationTargetException e) {
 			throw new RuntimeException(e.getMessage());
+		}
+	}
+
+	private void recuperaConta() {
+		Field[] fields = this.manipuladorDeContas.getClass().getFields();
+		for (Field field : fields) {
+			try {
+				Class<?> conta = Class.forName(CLASSE_CONTA);
+				if(field.getClass().isAssignableFrom(conta)) {
+					field.setAccessible(true);
+					populaTela(field.get(this.manipuladorDeContas));
+				}
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException("Não foi encontrada a classe " + CLASSE_CONTA + " no pacote " + PACOTE_MODELO + " Verifique se o pacote e o nome da classe estão corretos.");
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				throw new RuntimeException("Oops, problemas internos. Chame o instrutor. '-' \nException: " + e.getMessage());
+			}
+		}
+	}
+
+	private void populaTela(Object conta) {
+		for(String nome : Campos.getNomeDosCampos()) {
+			String nomeDoMetodo = "get" + StringUtils.capitalize(nome);
+			try {
+				Method getter = conta.getClass().getMethod(nomeDoMetodo);
+				Object retorno = getter.invoke(conta);
+				Campos.buscaCampo(nome).setText(retorno.toString());
+			} catch (NoSuchMethodException e) {
+				throw new RuntimeException("Não foi encontrado o método " + nomeDoMetodo + " dentro da classe " + CLASSE_CONTA + ". Verifique se o método foi criado corretamente.");
+			} catch (SecurityException | IllegalAccessException e) {
+				throw new RuntimeException("Não foi possível chamar o método " + nomeDoMetodo + " dentro da classe " + CLASSE_CONTA + ". Verifique se o método é público.");
+			} catch (IllegalArgumentException e) {
+				throw new RuntimeException("Oops, problemas internos. Chame o instrutor. '-' \nException: " + e.getMessage());
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e.getMessage());
+			}
 		}
 	}
 
--- src/br/com/caelum/javafx/api/util/Campos.java
+++ src/br/com/caelum/javafx/api/util/Campos.java
@@ -2,34 +2,39 @@ package br.com.caelum.javafx.api.util;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.Set;
 
-import br.com.caelum.javafx.api.controllers.ContasController;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import br.com.caelum.javafx.api.controllers.ContasController;
 
 public class Campos {
 	
-	private static Map<String, TextField> todosOsCampos;
+	private static Map<String, Label> todosOsCampos;
 
 	public static void registraCampos(ContasController controller){
 		Field[] atributos = controller.getClass().getFields();
 		for (Field atributo : atributos) {
-			if(atributo.getClass().isAssignableFrom(TextField.class) && atributo.isAnnotationPresent(FXML.class)){
+			if(atributo.getClass().isAssignableFrom(Label.class) && atributo.isAnnotationPresent(FXML.class)){
 				try {
 					atributo.setAccessible(true);
-					todosOsCampos.put(atributo.getName(), (TextField) atributo.get(controller));
+					todosOsCampos.put(atributo.getName(), (Label) atributo.get(controller));
 				} catch (IllegalArgumentException | IllegalAccessException e) {
-					throw new RuntimeException();
+					throw new RuntimeException("Oops, problemas internos. Chame o instrutor. '-' \nException: " + e.getMessage());
 				}
 			}
 		}
 	}
 	
-	public static TextField buscaCampo(String campo) {
-		TextField encontrado = todosOsCampos.get(campo);
+	public static Label buscaCampo(String campo) {
+		Label encontrado = todosOsCampos.get(campo);
 		if(encontrado == null){
 			throw new RuntimeException("Não foi encontrado o campo com o nome " + campo + ". Verifique se o nome está correto.");
 		}
 		return encontrado;
+	}
+
+	public static Set<String> getNomeDosCampos(){
+		return todosOsCampos.keySet();
 	}
 }
